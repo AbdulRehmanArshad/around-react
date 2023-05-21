4f0d3e26fdce2444d5a9b7ce67837822ba98df99
AbdulrehmanArshad <13pfye2046@gmail.com>
2023-05-21 08:01:25 +0500

    git changes

---
+++ src/components/AddPlacePopup.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import PopupWithForm from './PopupWithForm';
+import { useForm } from '../utils/useForm';
+
+const AddPlacePopup = ({ isLoading, isOpen, onClose, onAddPlaceSubmit }) => {
+  const { values, handleChange, setValues } = useForm({});
+  function handleSubmit(e) {
+    const { name, link } = values;
+
+    e.preventDefault();
+    onAddPlaceSubmit({ name, link });
+  }
+
+  useEffect(() => {
+    setValues({});
+  }, [isOpen, setValues]);
+
+  return (
+    <PopupWithForm
+      title="New place"
+      name="popup-place"
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText={isLoading ? 'Saving...' : 'Create'}
+      onSubmit={handleSubmit}
+    >
+      <fieldset className="form__fieldset">
+        <div className="form__input-container">
+          <input
+            id="place-title-input"
+            type="text"
+            name="name"
+            placeholder="Title"
+            className="form__input form__input_type_place-name"
+            required
+            minLength="2"
+            maxLength="30"
+            onChange={handleChange}
+            value={values.name || ''}
+            autoComplete="off"
+          />
+          <span className="form__input-error place-title-input-error"></span>
+        </div>
+        <div className="form__input-container">
+          <input
+            id="place-url-input"
+            type="url"
+            name="link"
+            placeholder="Image link"
+            className="form__input form__input_type_place-url"
+            required
+            value={values.link || ''}
+            onChange={handleChange}
+          />
+          <span className="form__input-error place-url-input-error" />
+        </div>
+      </fieldset>
+    </PopupWithForm>
+  );
+};
+
+export default AddPlacePopup;
---
+++ src/utils/api.js
@@ -0,0 +1,110 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error ${res.status}`);
+  }
+
+  getInitialCards(token) {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo(token) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    }).then(this._checkResponse);
+  }
+
+  setUserInfo({ name, about }, token) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        name: name,
+        about: about,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  createCard(data, token) {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
+
+  deleteCard(id, token) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      method: 'DELETE',
+    }).then(this._checkResponse);
+  }
+
+  changeLikeCardStatus(id, isLiked, token) {
+    if (!isLiked) {
+      return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+        method: 'PUT',
+      }).then(this._checkResponse);
+    } else {
+      return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+        method: 'DELETE',
+      }).then(this._checkResponse);
+    }
+  }
+
+  setUserAvatar(url, token) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        avatar: url,
+      }),
+    }).then(this._checkResponse);
+  }
+}
+
+let node_env = 'production';
+
+let baseUrl =
+  node_env === 'production'
+    ? 'https://vercel.com/matytsoraro/react-around-api-full'
+    : 'http://localhost:3000';
+const api = new Api({
+  baseUrl,
+});
+
+export default api;
